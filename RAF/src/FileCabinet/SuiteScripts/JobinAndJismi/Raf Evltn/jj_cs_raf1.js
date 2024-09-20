2a94fb57a3c0ffb25c1ccf01e8f7e3a0f47d7d71
shamiliqbaljj <shamil.iqbal@jobinandjismi.com>
2024-09-20 15:57:02 +0530

    Raf Evaluation Initial Commit

---
+++ RAF/src/FileCabinet/SuiteScripts/JobinAndJismi/Raf Evltn/jj_cs_raf1.js
@@ -0,0 +1,428 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+define(['N/record', 'N/search'],
+/**
+ * @param{record} record
+ * @param{search} search
+ */
+function(record, search) {
+    
+    /**
+     * Function to be executed after page is initialized.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.mode - The mode in which the record is being accessed (create, copy, or edit)
+     *
+     * @since 2015.2
+     */
+    function pageInit(scriptContext) {
+        
+        
+
+    }
+
+    /**
+     * Function to be executed when field is changed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+     * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+     *
+     * @since 2015.2
+     */
+    function fieldChanged(scriptContext) {
+        
+        try{
+            if (scriptContext.sublistId == 'salesordersublist')
+            {
+        let currentRecord = scriptContext.currentRecord
+        let itemName = currentRecord.getCurrentSublistText({
+            sublistId : 'salesordersublist',
+            fieldId : 'custpage_itemname'
+        });
+        
+
+
+        var mySearch = search.create({
+            title: "Item4",
+            type: search.Type.ITEM,
+            filters: ['name','is',itemName],
+            columns: ['salesdescription','baseprice']
+        });
+        let searchResult = mySearch.run().getRange({
+            start : 0,
+            end : 50
+        })
+
+        for (var i = 0; i < searchResult.length; i++) {
+            var description = searchResult[i].getValue({
+                name: 'salesdescription'
+            });
+            var basePrice = searchResult[i].getValue({
+                name: 'baseprice'
+            });
+            
+        }
+
+        currentRecord.setCurrentSublistValue({
+            sublistId: 'salesordersublist',
+            fieldId: 'custpage_description',
+            value: description,
+            ignoreFieldChange: true
+        });
+
+        currentRecord.setCurrentSublistValue({
+            sublistId: 'salesordersublist',
+            fieldId: 'custpage_price',
+            value: basePrice,
+            ignoreFieldChange: true
+        });
+    }
+}
+    catch(error)
+    {
+        log.debug(error)
+    }
+    
+    
+
+   
+
+        
+
+    }
+
+    /**
+     * Function to be executed when field is slaved.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     *
+     * @since 2015.2
+     */
+    function postSourcing(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed after sublist is inserted, removed, or edited.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @since 2015.2
+     */
+    function sublistChanged(scriptContext) {
+
+        
+
+    }
+
+    /**
+     * Function to be executed after line is selected.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @since 2015.2
+     */
+    function lineInit(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when field is changed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+     * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+     *
+     * @returns {boolean} Return true if field is valid
+     *
+     * @since 2015.2
+     */
+    function validateField(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when sublist line is committed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateLine(scriptContext) {
+
+        try{
+        let currentRecord = scriptContext.currentRecord;
+        let quantity = currentRecord.getCurrentSublistText({
+            sublistId : 'salesordersublist',
+            fieldId : 'custpage_quantity'
+        });
+        let basePrice = currentRecord.getCurrentSublistText({
+            sublistId : 'salesordersublist',
+            fieldId : 'custpage_price'
+        });
+        
+        let total = quantity * basePrice;
+
+    currentRecord.setCurrentSublistValue({
+        sublistId: 'salesordersublist',
+        fieldId: 'custpage_total',
+        value: total,
+        ignoreFieldChange: true
+    });
+}
+catch(error)
+{
+    log.debug(error)
+}
+return true;
+
+    }
+
+    /**
+     * Validation function to be executed when sublist line is inserted.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateInsert(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when record is deleted.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateDelete(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when record is saved.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @returns {boolean} Return true if record is valid
+     *
+     * @since 2015.2
+     */
+    function saveRecord(scriptContext) {
+
+        
+        let currentRecord = scriptContext.currentRecord;
+        let itemName = currentRecord.getSublistValue({
+            sublistId : 'salesordersublist',
+            fieldId : 'custpage_itemname',
+            line : 0
+        });
+        let itemquantity = currentRecord.getSublistValue({
+            sublistId : 'salesordersublist',
+            fieldId : 'custpage_quantity',
+            line : 0
+        });
+        let customerEmail = currentRecord.getText('email');
+        let customerName = currentRecord.getText('firstname');
+        
+        let quantity = currentRecord.getText('custpage_quantity')
+        
+        var mySearch = search.create({
+            title: "Customer 1",
+            type: search.Type.CUSTOMER,
+            filters: ['email','is',customerEmail],
+            columns: ['internalid']
+        });
+        let searchResult = mySearch.run().getRange({
+            start : 0,
+            end : 50
+        })
+
+        for (var i = 0; i < searchResult.length; i++) {
+            var internalId = searchResult[i].getValue({
+                name: 'internalid'
+            });
+
+        
+        }
+
+        
+        if (internalId)
+        {
+            //Creating Customer Record
+
+            let salesOrderRecord = record.create ({
+                type : record.Type.SALES_ORDER,
+                isDynamic : true
+            });
+
+            salesOrderRecord.setValue({
+                fieldId : 'entity',
+                value : internalId
+            });
+
+            salesOrderRecord.setCurrentSublistValue({
+                sublistId : 'item',
+                fieldId : 'item',
+                value : itemName
+            });
+            salesOrderRecord.setCurrentSublistValue({
+                sublistId : 'item',
+                fieldId : 'quantity',
+                value : itemquantity
+            });
+            salesOrderRecord.commitLine ({
+                sublistId : 'item'
+            });
+            let recordId = salesOrderRecord.save({
+                enableSourcing : false,
+                ignoreMandatoryFields : true
+            });
+
+            var mySearch = search.create({
+                title: "Total",
+                type: search.Type.SALES_ORDER,
+                filters: ['internalid','is',recordId],
+                columns: ['total','salesrep']
+            });
+            let searchResult = mySearch.run().getRange({
+                start : 0,
+                end : 50
+            })
+    
+            for (var i = 0; i < searchResult.length; i++) {
+                var total = searchResult[i].getValue({
+                    name: 'total'
+                });
+                var salesRep = searchResult[i].getValue({
+                    name: 'salesrep'
+                });
+            }
+
+            if(total > 500)
+            {
+                
+            var mySearch = search.create({
+                title: "salesRep",
+                type: search.Type.EMPLOYEE,
+                filters: ['internalid','is',salesRep],
+                columns: ['supervisor']
+            });
+            let searchResult = mySearch.run().getRange({
+                start : 0,
+                end : 50
+            });
+            for (var i = 0; i < searchResult.length; i++) {
+                var supervisorId = searchResult[i].getValue({
+                    name: 'supervisor'
+                });     
+            }
+            alert(supervisorId);
+            
+            }
+
+            
+        }
+        else
+        {
+            //Creating Customer Record
+
+            let customerRecord = record.create ({
+                type : record.Type.CUSTOMER,
+                isDynamic : true
+            });
+            customerRecord.setValue({
+                fieldId : 'companyname',
+                value : customerName
+            });
+            let customerRecordId = customerRecord.save({
+                enableSourcing : false,
+                ignoreMandatoryFields : true
+            });
+
+            // Creating Sales Order
+           
+            let salesOrderRecord = record.create ({
+                type : record.Type.SALES_ORDER,
+                isDynamic : true
+            });
+
+            salesOrderRecord.setValue({
+                fieldId : 'entity',
+                value : internalId
+            });
+
+            salesOrderRecord.setCurrentSublistValue({
+                sublistId : 'item',
+                fieldId : 'item',
+                value : itemName
+            });
+            salesOrderRecord.setCurrentSublistValue({
+                sublistId : 'item',
+                fieldId : 'quantity',
+                value : itemquantity
+            });
+            salesOrderRecord.commitLine ({
+                sublistId : 'item'
+            });
+            let salesRecordId = salesOrderRecord.save({
+                enableSourcing : false,
+                ignoreMandatoryFields : true
+            });
+            
+            
+            
+        }
+
+        
+
+
+    return true;
+
+}
+
+    return {
+        // pageInit: pageInit,
+        fieldChanged: fieldChanged,
+        // postSourcing: postSourcing,
+        // sublistChanged: sublistChanged,
+        // lineInit: lineInit,
+        // validateField: validateField,
+        validateLine: validateLine,
+        // validateInsert: validateInsert,
+        // validateDelete: validateDelete,
+        saveRecord: saveRecord
+    };
+    
+});
---
+++ RAF/src/FileCabinet/SuiteScripts/JobinAndJismi/Raf Evltn/jj_sl_raf.js
@@ -0,0 +1,109 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ */
+define(['N/ui/serverWidget'],
+    
+    (serverWidget) => {
+        /**
+         * Defines the Suitelet script trigger point.
+         * @param {Object} scriptContext
+         * @param {ServerRequest} scriptContext.request - Incoming request
+         * @param {ServerResponse} scriptContext.response - Suitelet response
+         * @since 2015.2
+         */
+        const onRequest = (scriptContext) => {
+            if (scriptContext.request.method === 'GET')
+                {
+                    try{
+                   createForm();
+                    }
+                    catch(error)
+                    {
+                        log,debug(error)
+                    }
+                   
+                    function createForm()
+                    {     
+                  try{
+                    var form = serverWidget.createForm({
+                    title: 'Sales Order',
+                    });
+    
+                    form.addField({
+                        id: 'firstname',
+                        type: serverWidget.FieldType.TEXT,
+                        label: 'First Name'
+                    }).isMandatory = true;
+                    form.addField({
+                        id: 'lastname',
+                        type: serverWidget.FieldType.TEXT,
+                        label: 'Last Name'
+                    }).isMandatory = true;
+                    form.addField({
+                        id: 'email',
+                        type: serverWidget.FieldType.EMAIL,
+                        label: 'E mail'
+                    }).isMandatory = true;
+                    form.addField({
+                        id: 'phonenumber',
+                        type: serverWidget.FieldType.PHONE,
+                        label: 'Phone Number'
+                    }).isMandatory = true;
+
+
+
+                    var subList = form.addSublist({
+                        id: 'salesordersublist',
+                        type: serverWidget.SublistType.INLINEEDITOR,
+                        label: 'Sales Order Sublist',
+                    });
+                    subList.addField({
+                        id: 'custpage_itemname',
+                        type: serverWidget.FieldType.SELECT,
+                        label: 'Item',
+                        source : 'item'
+                    });
+                    subList.addField({
+                        id: 'custpage_description',
+                        type: serverWidget.FieldType.TEXT,
+                        label: 'Description'
+                    });
+                    subList.addField({
+                        id: 'custpage_quantity',
+                        type: serverWidget.FieldType.INTEGER,
+                        label: 'Quantity'
+                    }).isMandatory = true;
+                    subList.addField({
+                        id: 'custpage_price',
+                        type: serverWidget.FieldType.FLOAT,
+                        label: 'Price'
+                    }).isMandatory = true;
+                    subList.addField({
+                        id: 'custpage_total',
+                        type: serverWidget.FieldType.FLOAT,
+                        label: 'Total'
+                    });
+                    form.addSubmitButton({
+                        label: "Submit"
+                    });
+                    scriptContext.response.writePage(form);
+                
+                    form.clientScriptModulePath = 'SuiteScripts/JobinAndJismi/Raf Evltn/jj_cs_raf1.js';
+                }
+            
+                catch(error)
+            {
+                log.debug(error)   
+            }
+        }
+            }
+        
+            
+
+
+        }
+
+        return {onRequest}
+
+    });
